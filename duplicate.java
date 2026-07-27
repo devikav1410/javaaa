af7b6edbfbcc01466d24698a009e7fdea2c382b1
DEVIKA V <devikav1410@gmail.com>
2026-07-27 11:05:04 +0530

    Update duplicate.java

--- duplicate.java
+++ duplicate.java
@@ -1,28 +1,23 @@
-import java.util.Scanner;
-
+  import java.util.Scanner;
 public class duplicate{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter the number of elements: ");
-        int n = scanner.nextInt();
-        
-        int[] array = new int[n];
-        System.out.println("Enter " + n + " elements:");
-        for (int i = 0; i < n; i++) {
-            array[i] = scanner.nextInt();
-        }
-        
-        System.out.print("Duplicate elements: ");
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j]) {
-                    System.out.print(array[j] + " ");
-                    break; 
-                }
-            }
-        }
-        
-        scanner.close();
-    }
+ public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);        
+ System.out.print("Enter the number of elements: ");
+int n = scanner.nextInt();       
+ int[] array = new int[n];
+System.out.println("Enter " + n + " elements:");
+for (int i = 0; i < n; i++) {
+array[i] = scanner.nextInt();
+}        
+ System.out.print("Duplicate elements: ");
+for (int i = 0; i < array.length; i++) {
+for (int j = i + 1; j < array.length; j++) {
+ if (array[i] == array[j]) {
+ System.out.print(array[j] + " ");
+ break; 
+}
+ }
+}        
+scanner.close();
+ }
 }
