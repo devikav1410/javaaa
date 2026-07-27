b5814a1ea7b0832127ed0d38721801e0d048b5c5
DEVIKA V <devikav1410@gmail.com>
2026-07-27 11:04:20 +0530

    Refactor array sorting program structure

--- arraysort.java
+++ arraysort.java
@@ -1,27 +1,18 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class SortAscendingBuiltIn {
+public class arraysort{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // 1. Get array size from user
+        Scanner scanner = new Scanner(System.in);       
         System.out.print("Enter the number of elements: ");
-        int size = scanner.nextInt();
-        
-        // 2. Initialize array and accept inputs
+        int size = scanner.nextInt();       
         int[] numbers = new int[size];
         System.out.println("Enter " + size + " integers:");
         for (int i = 0; i < size; i++) {
             numbers[i] = scanner.nextInt();
-        }
-        
-        // 3. Sort array using Java utility method
-        Arrays.sort(numbers);
-        
-        // 4. Print the sorted elements
-        System.out.println("Array in ascending order: " + Arrays.toString(numbers));
-        
+        }       
+        Arrays.sort(numbers);          
+        System.out.println("Array in ascending order: " + Arrays.toString(numbers));        
         scanner.close();
     }
 }
